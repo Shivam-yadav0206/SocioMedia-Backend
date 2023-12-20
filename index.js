b79a767ef350bc8d888316a544c58d996b95dba6
Shivam Yadav <sy7176442@gmail.com>
2023-12-21 04:42:23 +0530

    multe

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
-app.use("/tmp",express.static(path.join(__dirname, '/tmp')));
+app.use(
+  "/public/assets",
+  express.static(path.join(__dirname, "/public/assets"))
+);
 
 /*FILE STORAGE */
 const storage = multer.diskStorage({
@@ -39,7 +42,7 @@ const storage = multer.diskStorage({
         cb(null, "/tmp");
     },
     filename:function(req,file,cb){
-        cb(null,Date.now() + ' ' + file.originalname);
+        cb(null,file.originalname);
     },
 })
 const upload = multer({storage});
