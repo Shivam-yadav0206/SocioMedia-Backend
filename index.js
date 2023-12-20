8eefec48ffcd296a14e536084bcec0faeb5afd58
Shivam Yadav <sy7176442@gmail.com>
2023-12-21 04:49:16 +0530

    mul

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import {createPost} from "./controllers/posts.js";
-import {verifyToken} from "./middleware/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import {users, posts} from "./data/index.js";
+import { users, posts } from "./data/index.js";
 import { hostname } from "os";
 
 // CONFIGURATIOn
@@ -26,49 +26,46 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use(
-  "/public/assets",
-  express.static(path.join(__dirname, "/public/assets"))
-);
+app.use("/assets", express.static(path.join(__dirname, "/public/assets")));
 
 /*FILE STORAGE */
 const storage = multer.diskStorage({
-    destination: function(req,file,cb) {
-        cb(null, "/public/assets");
-    },
-    filename:function(req,file,cb){
-        cb(null,file.originalname);
-    },
-})
-const upload = multer({storage});
+  destination: function (req, file, cb) {
+    cb(null, "/public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 //ROUTES WITH FILES
-app.post("/auth/register", upload.single("picture"),register);
-app.post("/posts",verifyToken,upload.single("picture"),createPost);
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 //ROUTES
-app.use("/auth",authRoutes);
-app.use("/users",userRoutes);
-app.use("/posts",postRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
-app.get('/', (req,res) => {
-    res.send('Hello on SocioMedia backend');
-})
+app.get("/", (req, res) => {
+  res.send("Hello on SocioMedia backend");
+});
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3001;
 
 try {
-    mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology:true,
-    })
-    app.listen(PORT, () => console.log(`Server PORT: ${PORT}`));
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  app.listen(PORT, () => console.log(`Server PORT: ${PORT}`));
 } catch (error) {
-    console.log(`${error} did not connect`);
+  console.log(`${error} did not connect`);
 }
