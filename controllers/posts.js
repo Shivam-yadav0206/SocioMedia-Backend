9154576adbd476e592f820bfc3e9d65749cdb206
Shivam Yadav <sy7176442@gmail.com>
2023-12-20 21:33:14 +0530

    comments-trial

--- controllers/posts.js
+++ controllers/posts.js
@@ -72,3 +72,24 @@ export const likePost = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+
+export const commentPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId, comment } = req.body;
+    const post = await Post.findById(id);
+    const comments = post.comments;
+
+    comments.push(comment);
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { comments: post.comments },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
